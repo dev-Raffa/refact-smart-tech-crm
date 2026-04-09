6993760f98a14c6e046a635f6086f094caba4723
Rafael da Conceição <mnraffaell@gmail.com>
2026-04-09 17:04:17 -0300

    refact onUnautorized

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -6,8 +6,18 @@ export const bootstrapApp = () => {
   const getToken = () => useAuthStore.getState().token;
 
   setupHttpClientInterceptors(getToken, () => {
-    const isLoggingOut = useAuthStore.getState().status === 'unauthenticated';
-    if (!isLoggingOut) {
+    const { expiresAt } = useAuthStore.getState();
+
+    if (!expiresAt) {
+      window.dispatchEvent(new CustomEvent('on-session-expired'));
+      useAuthStore.getState().logout();
+      return;
+    }
+
+    const TokenExpiresAt = new Date(expiresAt);
+    const isExpired = TokenExpiresAt < new Date();
+
+    if (isExpired) {
       window.dispatchEvent(new CustomEvent('on-session-expired'));
       useAuthStore.getState().logout();
     }
